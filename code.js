931bc3a07757ad0de3e121e99413e792b701df76
Juan Carlos Ramirez <juancarlosramirezguerrero@gmail.com>
2022-11-27 13:05:38 +0100

    boton de despliegue (falta que se vea)

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(() => {
     $(".hamburger-lines").click(() => {
       $(".checkbtn").toggleClass("open");
       $("body").toggleClass("fixed_position");
     });
   
     // Cerrar menu al pulsar enlace
-    $(".menu a").click(() => {
+    $("ul a").click(() => {
       $(".checkbtn").removeClass("open");
       $("body").removeClass("fixed_position");
     });
